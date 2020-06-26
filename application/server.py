cf1b38efe0ba1f0dbf48fe780c72322cb81e0c34
Sisay Chala <chala@fit.fraunhofer.de>
2020-06-26 11:06:15 +0200

    endpoint for mobility request

--- application/server.py
+++ application/server.py
@@ -49,6 +49,6 @@ class MobilityRequest(Resource):
         return (data)
 
 if __name__ == '__main__':
-    db.drop_all()
-    db.create_all()
+    #db.drop_all()
+    #db.create_all()
     app.run(host='0.0.0.0', port=5003, debug=True)
