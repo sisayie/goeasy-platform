de68811915bd94f3b025e7a542340e7c012fd1d4
Sisay Chala <chala@fit.fraunhofer.de>
2020-09-17 11:38:42 +0200

    no recreate

--- application/server.py
+++ application/server.py
@@ -52,8 +52,6 @@ class MobilityRequest(Resource):
         return (data)
 
 if __name__ == '__main__':
-    db.drop_all()
-    db.create_all()
     logger.debug("Starting TPMMD threads !!!!")
     startTPMMD()
     logger.debug("This is just befor running app.run() !!!!")
