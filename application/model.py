1f170fabfb7c730f55c1bf6f35811f44ace7bdfa
Sisay Chala <chala@fit.fraunhofer.de>
2020-10-08 18:18:31 +0200

    bug fixes mobility modes

--- application/model.py
+++ application/model.py
@@ -74,8 +74,7 @@ class Journey(db.Model):
         #self.u_behaviour = str(u_behaviour)
         self.tpv_defined_behaviour = json.dumps(tpv_defined_behaviour)
         self.app_defined_behaviour = json.dumps(app_defined_behaviour)
-        #self.user_defined_behaviour = str(user_defined_behaviour)
-	self.user_defined_behaviour = json.dumps(user_defined_behaviour)
+        self.user_defined_behaviour = json.dumps(user_defined_behaviour)
         self.tpmmd = tpmmd
 #==========================================
 
